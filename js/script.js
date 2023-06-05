2d0314f84109f961eb9a46629fa6db16683943a0
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2023-06-05 17:52:46 -0300

    correçoes carrossel

--- js/script.js
+++ js/script.js
@@ -1,46 +1,50 @@
-const imgs = document.getElementsByClassName("carroseul-item");
-const img = document.querySelectorAll(".carroseul-item img");
+    // Obtenha os elementos do DOM
+    const carousel = document.querySelector('.carousel');
+    const slideContainer = carousel.querySelector('.carousel-container');
+    const slide = carousel.querySelector('.carousel-slide');
+    const prevBtn = carousel.querySelector('#prevBtn');
+    const nextBtn = carousel.querySelector('#nextBtn');
+    const slideWidth = slide.clientWidth;
+    let position = 0;
+    let transitioning = false;
 
-let idx = 0;
+    // Função para mover o carrossel
+    const moveCarousel = (amount) => {
+      if (!transitioning) {
+        transitioning = true;
+        position += amount;
+        slide.style.transition = `transform 0.5s ease-in-out`;
+        slide.style.transform = `translateX(-${position}px)`;
+      }
+    };
 
-function carrossel() {
-    idx++;
+    // Event listener para o botão anterior
+    prevBtn.addEventListener('click', () => {
+      if (position > 0) {
+        moveCarousel(-slideWidth);
+      }
+    });
 
-    if (idx > img.length - 1) {
-        idx = 0;
-    }
+    // Event listener para o botão próximo
+    nextBtn.addEventListener('click', () => {
+      if (position < (slide.scrollWidth - slideWidth)) {
+        moveCarousel(slideWidth);
+      }
+    });
 
-    imgs.style.transform = `translateX(${-idx * 500}px)`;
-}
+    // Resetar a posição do carrossel quando a transição terminar
+    slide.addEventListener('transitionend', () => {
+      if (position === slide.scrollWidth - slideWidth) {
+        position = 0;
+        slide.style.transition = `none`;
+        slide.style.transform = `translateX(0)`;
+      }
+      transitioning = false;
+    });
 
-setInterval(carrossel, 1);
-// CAROUSEL
-
-// Obtenha os elementos do DOM
-const carousel = document.querySelector('.carousel');
-const slideContainer = carousel.querySelector('.carousel-container');
-const slide = carousel.querySelector('.carousel-slide');
-const prevBtn = carousel.querySelector('#prevBtn');
-const nextBtn = carousel.querySelector('#nextBtn');
-const slideWidth = slide.clientWidth;
-
-// Defina a posição inicial do slide
-let position = 0;
-
-// Event listener para o botão anterior
-prevBtn.addEventListener('click', () => {
-  // Verifique se não está no primeiro slide
-  if (position > 0) {
-    position -= slideWidth;
-    slide.style.transform = `translateX(-${position}px)`;
-  }
-});
-
-// Event listener para o botão próximo
-nextBtn.addEventListener('click', () => {
-  // Verifique se não está no último slide
-  if (position < (slide.scrollWidth - slideWidth)) {
-    position += slideWidth;
-    slide.style.transform = `translateX(-${position}px)`;
-  }
-});
+    // Transição automática para o próximo slide a cada 5 segundos
+    setInterval(() => {
+      if (!transitioning && position < (slide.scrollWidth - slideWidth)) {
+        moveCarousel(slideWidth);
+      }
+    }, 5000);
